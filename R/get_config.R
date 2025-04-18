5912d50421aef13858eb0ee225a52a9d4beab3c5
Niklas Ippisch <n.ippisch@campus.lmu.de>
2025-04-18 07:13:56 +0200

    shortening function

--- R/get_config.R
+++ R/get_config.R
@@ -7,29 +7,9 @@
 #'
 
 .get_config <- function(setting) {
-  file_path <- file.path(tools::R_user_dir("rainer", which = "data"), setting)
 
-  if (!file.exists(file_path)) {
-    warning(sprintf("Configuration file '%s' does not exist.", setting))
-    stop(sprintf("Missing configuration for setting '%s'", setting))
-  }
+  file_path <- file.path(tools::R_user_dir("rainer", which = "data"), setting)
 
   do.call(Sys.setenv, stats::setNames(list(readLines(file_path)), setting))
 
-  if(setting == "rainer_token") {
-    token <- Sys.getenv("rainer_token")
-
-    if(github_token_validation(token)){
-      cat("Your token is still valid. \n")
-    } else {
-      cat("Your token is not valid. Maybe, it has expired. Please provide a new token.")
-
-      if (interactive()) {
-        cat("Please provide a new token.\n")
-        github_token()
-      } else {
-        warning("Invalid token, and session is non-interactive. Skipping token prompt.")
-      }
-    }
-  }
 }
